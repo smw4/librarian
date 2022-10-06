ed832b59bba18cb9edff381024e5792071bec875
smw4 <7003183+smw4@users.noreply.github.com>
2022-10-06 11:54:39 -0400

    Revised my code based on spec sheet. Must add a couple more methods. Then I'll recommit a post-walkthrough version of the project.

--- librarian.js
+++ librarian.js
@@ -22,11 +22,11 @@ If you’re looking for a challenge, try to create the four classes without usin
 */
 
 class Media {
-	constructor(title, isCheckedOut, ratings) {
+	constructor(title /* isCheckedOut, ratings*/) {
+		// I'm supposed to comment-out the second two arguments, as they are given default values. But when I do I get "not defined" error.
 		this._title = title;
-		this._isCheckedOut = isCheckedOut;
-		//this._isCheckedOut = false;
-		this._ratings = ratings;
+		this._isCheckedOut = false;
+		this._ratings = [];
 	}
 	get title() {
 		return this._title;
@@ -35,22 +35,33 @@ class Media {
 		return this._isCheckedOut;
 	}
 	get ratings() {
+		// a ratings array of integers
 		return this._ratings;
 	}
 
+	set isCheckedOut(keytwo) {
+		this._isCheckedOut = keytwo;
+	} // STEP 5 (NOT SURE ABOUT THIS ONE)
+
 	toggleCheckOutStatus() {
 		//change checked out status
-		if ((this._isCheckedOut = false)) {
-			this._isCheckedOut = true;
-		} else {
-			this._isCheckedOut = false;
-		}
+		this._isCheckedOut = !this._isCheckedOut; // CC's code "You can use the following syntax to negate the value saved to a boolean."
+	}
+
+	getAverageRating() {
+		//return the average value of the ratings array. Use the reduce method to find the sum of the ratings array. Divide this sum by the length of the ratings array, and return the result.
+	}
+	addRating() {
+		//a method that takes one argument and uses .push to add it to the end of the ratings array
+		//Make sure input is between 1 and 5.
 	}
 }
 
 class Book extends Media {
-	constructor(author, title, pages, isCheckedOut, ratings) {
-		super(title, isCheckedOut, ratings);
+	constructor(author, title, pages /*isCheckedOut, ratings*/) {
+		super(title /*isCheckedOut, ratings*/);
+		this._isCheckedOut;
+		this._ratings;
 		this._author = author;
 		this._pages = pages;
 	}
@@ -66,8 +77,10 @@ class Book extends Media {
 }
 
 class Movie extends Media {
-	constructor(director, title, runTime, isCheckedOut, ratings) {
-		super(title, isCheckedOut, ratings);
+	constructor(director, title, runTime /*isCheckedOut, ratings*/) {
+		super(title /*isCheckedOut, ratings*/);
+		this._isCheckedOut;
+		this._ratings;
 		this._director = director;
 		this._runTime = runTime;
 	}
@@ -80,6 +93,10 @@ class Movie extends Media {
 	}
 	//get isCheckedOut() {}
 	//get ratings() {}
+	toggleCheckOutStatus() {
+		// Step 14
+	}
+	getAverageRating() {}
 }
 
 class CD extends Media {
@@ -100,17 +117,18 @@ class CD extends Media {
 }
 
 //TESTING BOOK
-const JATGP = new Book('Roald Dahl', 'James and the Giant Peach', 144, true, [
-	'Good',
-	'Great',
-]);
-//console.log(JATGP.isCheckedOut); //isCheckedOut NOT WORKING. Always returns false, even when indicated true
+const historyOfEverything = new Book(
+	'Bill Bryson',
+	'A Short History of Nearly Everything',
+	544
+);
 
+historyOfEverything.toggleCheckOutStatus(); //Step 16
+console.log(historyOfEverything.isCheckedOut); //Step 17
 
 //TESTING MOVIE
 const bigFish = new Movie('Tim Burton', 'Big Fish', 120, false, 5);
 //console.log(bigFish.ratings);   //ratings works as an integer
-
 
 //TESTING CD
 const Adele19 = new CD('Adele', '19', true, 10, ['Crazy for You', 'Heartache']);
@@ -121,3 +139,7 @@ console.log(Adele19.songs);
 - isCheckedOut returns false even though I've indicated "true" (STATIC helps here?)
     -update: commenting out ''this._isCheckedOut = false;'' in Media constructor, has it working again...
 */
+
+//Extra Credit:
+//Create a method called shuffle for the CD class. The method returns a randomly sorted array of all the songs in the songs property.
+//Create class called Catalog that holds all of the Media items in our library.
